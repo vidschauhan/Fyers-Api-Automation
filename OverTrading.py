9b9f659377c1527c5cb1d529bc8cfdcdd1f47142
vidschauhan <vidschauhan@gmail.com>
2024-11-01 19:14:05 +0530

    minor fixes

--- OverTrading.py
+++ OverTrading.py
@@ -27,7 +27,8 @@ def get_position_details() :
     return details['overall']['count_open'],details['overall']['pl_total'],details['overall']['pl_realized'],details['overall']['pl_unrealized'],details['overall']['count_total']
 
 def over_trading_analyser():
-    print("Inside -> over_trading_analyser()")
+    print("\nInside -> over_trading_analyser()")
+    print(f"\nToday's Max Loss Limit is :: {daily_limit}\n")
 
 
     #returns details['overall']['count_open'],details['overall']['pl_total'],details['overall']['pl_realized'],details['overall']['pl_unrealized']
@@ -39,10 +40,12 @@ def over_trading_analyser():
     pl_unrealized = positions[3]
     count_total = positions[4]
     daily_limit_threshold = daily_limit - (0.1 * daily_limit)
-    pl_total = -1872.17
-    pl_realized = -3244.23
-    count_open = 1
+    #pl_total = -1872.17
+    #pl_realized = -3244.23
+    #count_open = 1
+    #count_total = 10
     MB_ICONWARNING = 0x00000030
+    WS_EX_TOPMOST = 0x40000
 
     daily_limit_reached_message = (' ******** You have reached daily limit of loss ********* \n\n'
                                    f'MAX LOSS LIMIT {daily_limit} || REALIZED P&L {pl_realized}'
@@ -58,7 +61,7 @@ def over_trading_analyser():
     daily_limit_threshold_message = ('******************* WARNING ******************'
                                      '\n\nSTAY DISCIPLINED. SMALL LOSSES CAN BE RECOVERED.'
                                      '\n\nYou have crossed the threshold loss.'
-                                     f'\n\n THRESHOLD : {daily_limit_threshold} TOTAL LOSS NOW : {pl_total}'
+                                     f'\n\nTHRESHOLD: {daily_limit_threshold} | TOTAL LOSS NOW : {pl_total}'
                                      '\n\nEither close existing trade or Stop Taking more Trades.'
                                      '\n\nREMEMBER : YOU HAVE TO ESCAPE 9-5 JOB, STAY FOCUSED')
 
@@ -69,6 +72,44 @@ def over_trading_analyser():
                           '\n\nREMEMBER : YOU HAVE TO ESCAPE 9-5 JOB, STAY FOCUSED'
                           '\n\n*********************************************************')
 
+    over_trading_detected = ('############ OVER TRADING DETECTED ############'
+                             f'\n\nTotal Number of Trades Today :: {count_total}'
+                             f'\nTotal Number of Running Trades :: {count_open}'
+                             f'\n\n1.If You are in PROFIT then take this profit Home.'
+                             '\n2.If You are in LOSS then time to close the terminal.'
+                             '\n3.Remember,The goal is to Survive in the Market'
+                             '\n4.You will always get the better opportunities'
+                             '\n5.BE A SNIPER TRADER not MACHINE GUNNER.'
+                             '\n\nTRADE YOUR PLAN & STICK TO IT'
+                             '\n\n############################################')
+
+    trading_awareness = ('############ ONLY TRADE YOUR SETUP ############'
+                             f'\n\nALWAYS TRADE YOUR PLAN. STAY DISCIPLINED'
+                             f'\nTRADE ONLY A & A+ SETUPS -> LIQUIDITY SWEEP <-'
+                             f'\n\n* WAIT FOR YOUR SETUP PATIENTLY.'
+                             '\n* LOSS DUE TO BAD DISCIPLINE DESTROY PSYCHOLOGY.'
+                             '\n* BIG LOSS GIVES YOU BIG PAIN.'
+                             '\n* ALWAYS KEEP YOUR LOSS IN CONTROL.'
+                             '\n* REMEMBER : LESS IS MORE.'
+                             '\n* PRESERVE YOUR CAPITAL FROM UNNECESSARY RISKS.'
+                             '\n\nPROPER RISK MANAGEMENT WILL SAVE YOU IN MARKET.'
+                             '\n\n############################################')
+
+
+
+    # Trading awareness so that I am trading my plan only.
+    if count_total == 0 and pl_total == 0 :
+        print("No Trading position for now.")
+        popup.AutoCloseMessageBoxW(trading_awareness, 'TRADING PSYCHOLOGY',60, MB_ICONWARNING)
+        return
+
+    #Over trading alert if number of all position > 10
+    if count_total >= 10 :
+        print("Over trading Detected.")
+        popup.AutoCloseMessageBoxW(over_trading_detected, f'OVER TRADING ALERT ! Total P&L : {pl_total}',
+                                   30, MB_ICONWARNING)
+
+
     if pl_total < 0 :
 
         if pl_total > daily_limit :
@@ -76,10 +117,10 @@ def over_trading_analyser():
             while count <= 5 and pl_realized > daily_limit:
                 if pl_total <= daily_limit_threshold:
                     popup.AutoCloseMessageBoxW(daily_limit_threshold_message, f'LOSS THRESHOLD CROSSED !  Total Loss : {pl_total}',
-                                         10, MB_ICONWARNING)
+                                         15, MB_ICONWARNING)
                     if count_open > 0:
-                        popup.AutoCloseMessageBoxW(dont_trade_anymore,f'DO NOT TRADE ANYMORE ! Total Loss : {pl_total}',10, MB_ICONWARNING)
-                sleep(10)
+                        popup.AutoCloseMessageBoxW(dont_trade_anymore,f'DO NOT TRADE ANYMORE ! Total Loss : {pl_total}',15, MB_ICONWARNING)
+                sleep(30)
                 count = count + 1
 
 
@@ -90,15 +131,28 @@ def over_trading_analyser():
                 if count_open > 0:
                     popup.AutoCloseMessageBoxW(dont_trade_anymore, f'DO NOT TRADE ANYMORE ! Total Loss : {pl_total}', 20,
                                                16)
-                    sleep(10)
-                    count = count + 1
+                sleep(30)
+                count = count + 1
 
         over_trading_analyser()
 
 
+def execute():
+    global daily_limit
+    try:
+        while True:
+            try:
+                daily_limit = -abs(float(input("\nEnter today's Loss Limit of the Day ::  ")))
+                break
+            except ValueError:
+                print("Please input a valid amount in numbers.")
+
+        while True :
+            over_trading_analyser()
+            sleep(300)
+    except:
+        print("Error Encountered. Retry...")
 
 
 if __name__ == '__main__':
-    print("Enter today's Loss Limit of the Day :: ")
-    daily_limit = -abs(float(input()))
-    over_trading_analyser()
+    execute()
